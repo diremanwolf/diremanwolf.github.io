f4e80b0ec1ac5a56f949a16b4b6acae60dfca195
Dante Samael Fernandez Lago <dante.fernandez@globant.com>
2020-09-23 22:32:17 -0500

    MODIFY INDEX.JS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ button.addEventListener('click', async () => {
     let device;
 
     try {
-        device = await navigator.usb.requestDevice({ filters: [{ vendorId: 0x079b }] })
+        device = await navigator.usb.requestDevice({ filters: [{ vendorId: 0x04E8 }] })
         console.info(connectedDevice);
     } catch(error) {
         console.error(error);
