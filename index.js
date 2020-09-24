41e9f44af2a6b489c21621406614b055c7eff333
Dante Samael Fernandez Lago <dante.fernandez@globant.com>
2020-09-24 04:54:21 -0500

    MODIFY INDEX.JS

--- index.js
+++ index.js
@@ -32,26 +32,21 @@ button.addEventListener('click', async () => {
             index: 0x00
         });
         console.info('trasnferring', selectedDevice)
-
-        values.set([0x6d, 0x65, 0x6d]);
-        result = await selectedDevice.transferOut(0, values.buffer)
-        console.log('mem:', result)
     
         const timeoutID = window.setTimeout(async() => {
             console.warn('Device not connected')
             await close()
         }, 5000);
 
-        while(true) {
-            let incoming = await selectedDevice.transferIn(0x01, 1024)
-            if (incoming.data.byteLength > 0) {
-                clearTimeout(timeoutID)
-                let decoder = new TextDecoder() // eslint-disable-line no-undef
-                const data = decoder.decode(incoming.data)
-                console.log(data)
-                if (data.includes('END')) {
-                break;
-                }
+        let incoming = await selectedDevice.transferIn(1, 1024)
+        if (incoming.data.byteLength > 0) {
+            clearTimeout(timeoutID)
+            let decoder = new TextDecoder() // eslint-disable-line no-undef
+            const data = decoder.decode(incoming.data)
+            console.log(data)
+            if (data.includes('END')) {
+                console.info('stops');
+                return false;
             }
         }
     } catch (error) {
