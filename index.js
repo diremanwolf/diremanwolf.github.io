27df39cf616abe18976126f936b7d73f60f73ca8
Dante Samael Fernandez Lago <dante.fernandez@globant.com>
2020-09-23 22:35:09 -0500

    MODIFY INDEX.JS

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const button = document.getElementById('request-device');
 
-button.addEventListener('click', async () => {
-    let device;
-
-    try {
-        device = await navigator.usb.requestDevice({ filters: [{ vendorId: 0x04E8 }] })
-        console.info(connectedDevice);
-    } catch(error) {
-        console.error(error);
-    }
+button.addEventListener('click', () => {
+    navigator.usb.getDevices().then(devices => {
+        devices.map(device => {
+          console.log(device.productName);      // "Arduino Micro"
+          console.log(device.manufacturerName); // "Arduino LLC"
+        });
+    })
 });
