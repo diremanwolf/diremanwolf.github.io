ba32541f0531e01033f91c36f48eed24550ea93f
Dante Samael Fernandez Lago <dante.fernandez@globant.com>
2020-09-23 23:01:27 -0500

    MODIFY INDEX.HTML

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const button = document.getElementById('request-device');
 
-const devices = [
-    { name: 'MORPHO', vendorId: 0, productId: 0, serialNumber: '1420I002904', classCode: 255, subclassCode: 255, protocolCode: 255 }
-]
-
 button.addEventListener('click', async () => {
     let device;
 
     try {
-        device = await navigator.usb.requestDevice({ filters: devices })
+        device = await navigator.usb.requestDevice({ filters: [{ serialNumber: '1420I002904' }] })
         console.info(device);
     } catch(error) {
         console.error(error);
