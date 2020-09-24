2b8639ad3498dd375a60b4829d0ccac15d3dc2e8
Dante Samael Fernandez Lago <dante.fernandez@globant.com>
2020-09-23 23:05:56 -0500

    ADD INDEX.JS, MODIFY INDEX.HTML

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ button.addEventListener('click', async () => {
 
     try {
         device = await navigator.usb.requestDevice({ filters: [{ serialNumber: '1420I002904' }] })
-        console.info(device);
+        
+        await device.open();
+        await device.selectConfiguration(1);
+        await device.controlTransferOut({ requestType: 'class', recipient: 'interface', request: 0x22, value: 0x01, index: 0x02 });
+    
+        let result = device.transferIn(5, 64);
+        let decoder = new TextDecoder();
+        document.getElementById('information-device').innerHTML = 'Received: ' + decoder.decode(result.data);
+    
     } catch(error) {
         console.error(error);
     }
