cf9614e27eaa8853ff22e948de23e4bb032a600a
Dante Samael Fernandez Lago <dante.fernandez@globant.com>
2020-09-24 05:09:18 -0500

    MODIFY INDEX.JS

--- index.js
+++ index.js
@@ -10,52 +10,29 @@ const config = new Uint8Array(10);
 button.addEventListener('click', async () => {
     let selectedDevice;
 
-    const closePort = {
-        requestType: 'vendor',
-        recipient: 'device',
-        request: 0x07,
-        value: 0x00,
-        index: 0x03
-    }
-
-    async function close () {
-        let result = await selectedDevice.controlTransferOut(closePort)
-        console.log('close port:', result)
-        await selectedDevice.releaseInterface(1)
-        await selectedDevice.close()
-    }
-
     try {
         selectedDevice = await navigator.usb.requestDevice({
             filters: device
-        })
-        console.info('opening', selectedDevice);
+        });
 
         await selectedDevice.open();
-        console.info('opened', selectedDevice);
 
         await selectedDevice.selectConfiguration(1);
         
         await selectedDevice.claimInterface(1);
-        console.info('interface', selectedDevice)
 
         await selectedDevice.controlTransferOut({
-            requestType: 'standard',
-            recipient: 'device',
-            request: 0x01,
-            value: 0x03,
-            index: 0x00
+            requestType: 'vendor',
+            recipient: 'interface',
+            request: 1,
+            value: 3,
+            index: 1
         });
-        console.info('trasnferring', selectedDevice)
-    
-        const timeoutID = window.setTimeout(async() => {
-            console.warn('Device not connected')
-            await close()
-        }, 5000);
 
-        let incoming = await selectedDevice.transferIn(0x01, 1024)
+        console.info('AFK: ', selectedDevice)
+    
+        let incoming = await selectedDevice.transferIn(1, 6)
         if (incoming.data.byteLength > 0) {
-            clearTimeout(timeoutID)
             let decoder = new TextDecoder() // eslint-disable-line no-undef
             const data = decoder.decode(incoming.data)
             console.log(data)
