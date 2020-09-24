6bd305a3551c0bd2a13ad405d521b67785a4567c
Dante Samael Fernandez Lago <dante.fernandez@globant.com>
2020-09-24 06:10:35 -0500

    MODIFY INDEX.JS

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ button.addEventListener('click', async () => {
         console.info(result);
 
         while(true) {
-            let result = await data.transferIn(1, 6);
+            let result = await data.transferIn(0x01, 1024);
             console.info(result);
         }
     } catch (error) {
