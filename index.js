9dda6c59a68420849ba37685a8529b6cb6b8b91c
Dante Samael Fernandez Lago <dante.fernandez@globant.com>
2020-09-24 05:56:47 -0500

    MODIFY INDEX.JS

--- index.js
+++ index.js
@@ -10,11 +10,35 @@ const config = new Uint8Array(10);
 button.addEventListener('click', async () => {
     let selectedDevice;
 
+    const startPort = {
+        requestType: 'vendor',
+        recipient: 'device',
+        request: 0x08,
+        value: 0x00,
+        index: 0x03
+      }
+
     const openPort = {
         requestType: 'vendor',
         recipient: 'device',
         request: 0x06,
         value: 0x89,
+        index: 0x03
+    }
+
+    const setPortConfig = {
+        requestType: 'vendor',
+        recipient: 'device',
+        request: 0x05,
+        value: 0x00,
+        index: 0x03
+    }
+
+    const closePort = {
+        requestType: 'vendor',
+        recipient: 'device',
+        request: 0x07,
+        value: 0x00,
         index: 0x03
     }
 
@@ -30,7 +54,10 @@ button.addEventListener('click', async () => {
         console.info(selectedDevice.configuration.interfaces)
 
         let result = await selectedDevice.controlTransferOut(openPort)
-        console.info(result)
+        console.info(result);
+
+        result = await selectedDevice.controlTransferOut(startPort)
+        console.info(result);
     } catch (error) {
         console.error(error);
     }
