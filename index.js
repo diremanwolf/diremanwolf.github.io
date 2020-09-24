e13d3581765a4d5f087351577e9bab8074c7ad9c
Dante Samael Fernandez Lago <dante.fernandez@globant.com>
2020-09-24 06:42:01 -0500

    MODIFY INDEX.JS

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ button.addEventListener('click', async () => {
 
 
         values.set([0x6d, 0x65, 0x6d])
-        result = await selectedDevice.transferOut(0x01, values.buffer)
+        result = await selectedDevice.transferOut(0x02, values.buffer)
 
         do {
             result = await selectedDevice.transferIn(0x03, 1024);
